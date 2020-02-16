4a0816f575d875b99e487d267a0c568b970ef24b
kidkai25 <40673009+kidkai25@users.noreply.github.com>
2020-02-16 18:40:42 +0530

    Add project files.

---
+++ StringsAndCharacters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringsAndCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
